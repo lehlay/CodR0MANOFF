a7a81fc6bc704b73c18e524e9db3ab49a2336a03
Teka <29285973+leticia321@users.noreply.github.com>
2019-12-11 18:12:47 -0300

    Create NovaIA.c

---
+++ NovaIA.c
@@ -0,0 +1,6 @@
+// O que ela vai fazer: Analizar todas as ações e calcular qual ação é mais vantajosa
+// // ANALIZAR e CALCULAR
+//  Ela vai considerar que a porcentagem é 100% 
+//  Divide o (qt. dinheiro em caixa)/preco --> vai indicar quantas daquelas ações podem ser compradas
+// Multiplica o numero de ações compraveis pelo lucro
+// Avaliar qual o maior montante de lucro --> essa será a jogada da IA
