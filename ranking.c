80df6196a89f4158e336d52e3446c63d5e6707f7
Teka <29285973+leticia321@users.noreply.github.com>
2019-12-12 12:50:49 -0300

    Update ranking.c

--- ranking.c
+++ ranking.c
@@ -6,57 +6,137 @@
 
 
 void ranking(double pontuacao, char* nome, Rkg placar){
- printf("teste aisjaosjiaopjsopa");
 
     FILE *record;
-    int i, a;
+    int i, a, controle;
     double tempP;
     char tempN[50];
 
     record = fopen("records.txt", "r");
- printf("teste aisjaosjiaopjsopa");
 
     for (i=0; i<5; i++){
         fgets(tempN,50, record);
         printf("%s", tempN);
-        fscanf (record, "%lf", &placar.p[i]);
+        fscanf (record, "%lf ", &placar.p[i]);
         printf("%lf", placar.p[i]);
-    }
+        printf("\n");
 
-   rewind(record);
-
-    for (i = 0; i<5; i++){
-        if ((placar.p[i]) <=pontuacao){
-            tempP = placar.p[i];
-            strcpy (tempN, placar.n[i][0]);
-            strcpy (placar.n[i][0], nome);
-            placar.p[i] = pontuacao;
-
-            for ( a = 5; a>(i+1); a++){
-                strcpy (placar.n[a][0], placar.n[a-1][0]);
-               placar. p[a] = placar.p[a-1];
-            }
-
-            strcpy (placar.n[i+1][0], tempN);
-            placar.p[i+1] = tempP;
-            break;
-        }
-    }
-
-    rewind(record);
-
-
-    //escrevendo a pontuação e o nome no arquivo
-    for (i=0; i<5; i++){
-         fprintf(record,"%s", placar.n[i][0]);
-         fprintf(record, "%lf", placar.p[i]);
     }
 
     fclose(record);
+    record = fopen("records.txt", "r");
+    rewind(record);
+
+
+   //Identificar uma pontuação menor que a do player
+    for (i = 0; i<5; i++){
+        if (placar.p[i] <= pontuacao){
+            controle = i;
+            break;
+        }
+    }
+    fclose(record);
+    rewind(record);
+
+    switch (controle){
+        case 0: {
+            // Salvo o elemento que será substituido e incluo o player no ranking
+            tempP = placar.p[i];
+            strcpy (tempN, placar.n1);
+            strcpy (placar.n1, nome);
+            placar.p[i] = pontuacao;
+            // Subistituir as outras posições
+            for (a = 5; a>(i+1); a++){
+                placar. p[a] = placar.p[a-1];
+            }
+            strcpy (placar.n5, placar.n4);
+            strcpy (placar.n4, placar.n3);
+            strcpy (placar.n3, placar.n2);
+            strcpy (placar.n2, tempN);
+            placar.p[i+1] = tempP;
+            break;
+        }
+        case 1: {
+            // Salvo o elemento que será substituido e incluo o player no ranking
+            tempP = placar.p[i];
+            strcpy (tempN, placar.n2);
+            strcpy (placar.n2, nome);
+            placar.p[i] = pontuacao;
+            // Subistituir as outras posições
+            for (a = 5; a>(i+1); a++){
+                placar. p[a] = placar.p[a-1];
+            }
+            strcpy (placar.n5, placar.n4);
+            strcpy (placar.n4, placar.n3);
+            strcpy (placar.n3, tempN);
+            placar.p[i+1] = tempP;
+            break;
+        }
+         case 2: {
+            // Salvo o elemento que será substituido e incluo o player no ranking
+            tempP = placar.p[i];
+            strcpy (tempN, placar.n3);
+            strcpy (placar.n3, nome);
+            placar.p[i] = pontuacao;
+            // Subistituir as outras posições
+            for (a = 5; a>(i+1); a++){
+                placar. p[a] = placar.p[a-1];
+            }
+            strcpy (placar.n5, placar.n4);
+            strcpy (placar.n4, tempN);
+            placar.p[i+1] = tempP;
+            break;
+        }
+         case 3: {
+                // Salvo o elemento que será substituido e incluo o player no ranking
+                tempP = placar.p[i];
+                strcpy (tempN, placar.n4);
+                strcpy (placar.n4, nome);
+                placar.p[i] = pontuacao;
+                // Subistituir as outras posições
+                for (a = 5; a>(i+1); a++){
+                    placar. p[a] = placar.p[a-1];
+                }
+                strcpy (placar.n5, tempN);
+                placar.p[i+1] = tempP;
+                break;
+            }
+
+
+         case 4: {
+                // Salvo o elemento que será substituido e incluo o player no ranking
+                strcpy (placar.n5, nome);
+                placar.p[i] = pontuacao;
+                break;
+            }
+        default: {
+            printf(" Jogador não entrou no ranking");
+            }
+    }
+
+    rewind(record);
+    record = fopen("records.txt", "w");
+    //escrevendo a pontuação e o nome no arquivo
+     fprintf(record,"%s", placar.n1);
+     fprintf(record, "%lf", placar.p[0]);
+     fprintf(record,"%s", placar.n2);
+     fprintf(record, "%lf", placar.p[1]);
+     fprintf(record,"%s", placar.n3);
+     fprintf(record, "%lf", placar.p[2]);
+     fprintf(record,"%s", placar.n4);
+     fprintf(record, "%lf", placar.p[3]);
+     fprintf(record,"%s", placar.n5);
+     fprintf(record, "%lf", placar.p[4]);
+
+     fclose(record);
+     rewind(record);
+     record = fopen("records.txt", "r");
 
     for (i=0; i<5; i++){
-        printf("%s", placar.n[i][0]);
-        printf("%lf", placar.p[i]);
+        fgets(tempN,50, record);
+        printf("%s", tempN);
+        fscanf (record, "%lf", &tempP);
+        printf("%lf",tempP);
     }
 
 
